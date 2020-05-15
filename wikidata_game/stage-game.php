c103f1229f5ace15578dfa399b8b16f2ecd2efee
Itamar Givon <itamar.givon@wikimedia.de>
2020-05-15 18:55:41 +0200

    WIP stage hook

--- wikidata_game/stage-game.php
+++ wikidata_game/stage-game.php
@@ -17,21 +17,21 @@
     $is_game_branch = substr($branch, 0, 5) === 'game-';
 
     // If it is not a merge to master from game branch, exit
-    // if($payload->action != 'synchronize'
-    //     || !$is_game_branch 
-    //     || $payload->pull_request->base->ref != 'master'){
+    if($payload->action != 'synchronize'
+        || !$is_game_branch 
+        || $payload->pull_request->base->ref != 'master'){
 
-    //     $message = $payload->action . ': ' . $payload->pull_request->base->ref . ': ' . $branch . ': ' . $payload->pull_request->merged;
-    //     file_put_contents($logfile_path, $datetime_string . ' ' . $message . PHP_EOL, FILE_APPEND);
+        $message = $payload->action . ': ' . $payload->pull_request->base->ref . ': ' . $branch . ': ' . $payload->pull_request->merged;
+        file_put_contents($logfile_path, $datetime_string . ' ' . $message . PHP_EOL, FILE_APPEND);
 
-    //     exit;
-    // }
+        exit;
+    }
 
     $script_path = '../reference-island/wikidata_game/stage.sh';
 
     $output = shell_exec($script_path . ' ' . $branch);
 
-    $message = 'Attempting to deploy ' . $payload->pull_request->head->sha . ' of ' . $payload->pull_request->head->ref . PHP_EOL;
+    $message = 'Attempting to deploy ' . $payload->pull_request->head->sha . ' of ' . $payload->pull_request->head->ref . ' to staging environment.' . PHP_EOL;
     $message .= $output ? $output : 'An error occurred while trying to stage' . $branch . '. Please see ~/error.log for more information.';
     
     file_put_contents($logfile_path, $datetime_string . ' ' . $message . PHP_EOL, FILE_APPEND);
