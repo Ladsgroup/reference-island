9ec2b35c8762654a0661f60902ed15bbcfd2717f
Itamar Givon <itamar.givon@wikimedia.de>
2020-05-15 18:11:44 +0200

    Add stage hook

---
+++ wikidata_game/stage.sh
@@ -0,0 +1,12 @@
+BRANCH=$1
+HOME="/data/project/wd-ref-island"
+
+git -C "$HOME/reference-island/" checkout $BRANCH
+git -C "$HOME/reference-island/" pull
+
+if [ ! -d "$HOME/public_html/stage" ]; then
+    mkdir "stage"
+fi
+
+cp "$HOME/reference-island/wikidata_game/api.php" "$HOME/public_html/stage/$BRANCH-api.php"
+echo "$BRANCH-api.php updated successfuly"
