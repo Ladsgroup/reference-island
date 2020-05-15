ecbbd3bafc760d4d1765eac17909c0e18b78f290
Itamar Givon <itamar.givon@wikimedia.de>
2020-05-15 13:01:08 +0200

    Add publish hook

--- wikidata_game/deploy.sh
+++ wikidata_game/deploy.sh
@@ -1,1 +1,6 @@
-cp ~/reference-island/wikidata_game/api.php ~/public_html/api.php
+HOME="/data/project/wd-ref-island"
+
+git -C "$HOME/reference-island/" checkout master
+git -C "$HOME/reference-island/" pull
+cp "$HOME/reference-island/wikidata_game/api.php" "$HOME/public_html/api.php"
+echo 'api.php updated successfuly'
